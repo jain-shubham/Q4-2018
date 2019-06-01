7b5e2c8d36f2d1f57a5207ec1a56b19d39e40cab
Shubham Jain <shubham.jain@metacube.com>
2019-06-01 12:19:21 +0530

    Uploading WPF projectc

---
+++ ExpenseIT/DemoHome.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpenseIT
+{
+    /// <summary>
+    /// Interaction logic for DemoHome.xaml
+    /// </summary>
+    public partial class DemoHome : Page
+    {
+        public DemoHome()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewReport(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            ExpenseReport expenseReportPage = new ExpenseReport(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+
+        }
+    }
+}
